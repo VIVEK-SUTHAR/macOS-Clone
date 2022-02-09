650d906788fcfb3dd854a2e23d7bd91f707b425e
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-02-09 19:20:53 +0530

    Minimize Button Added(Working)

--- JS/menu.js
+++ JS/menu.js
@@ -27,25 +27,28 @@ $('#close').click(function () {
     $('#finder-app').slideToggle(400);
 });
 $('#maximize').click(function () {
-    $('#finder-app').addClass("animate__zoomInDown");
+    // $('#finder-app').addClass("animate__zoomInDown");
 
 
 })
+let count_app = 0;
 $("#maximize").click(function () {
-    let count = 0;
-    if (count == 0) {
-        $("#finder-app").css("width", "+=350");
-        $("#finder-app").css("height", "+=180");
-        $("#finder-app").css("left", "100px");
-        $("#finder-app").css("top", "50px");
-        $(this).css("transition", "2000ms");
-        //$("finder-app").css('background-color','yellow');
-        count++;
+    if (count_app == 0) {
+        $("#finder-app").css("left", "0.5px");
+        $("#finder-app").css("top", "22px");
+        $("#finder-app").css("width", "+=659");
+        $("#finder-app").css("height", "+=260");
+        $("#finder-app").css("border-radius", "5px");
+        $("#finder-app").css("transition", "all 0.2s ease-in-out");
+        count_app++;
     }
-
-
-
-
+});
+$("#minimize").click(function () {
+    if (count_app == 1) {
+        $('#finder-app').addClass("animate__zoomOut");
+        $('#finder-app').slideToggle(400);
+        count_app++;
+    }
 });
 // $('#maximize').click( function (){
 // document.getElementById("finder-app").style = "height:580px;width:1200px";
