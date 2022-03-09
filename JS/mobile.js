a714003249890364ba7868fd33e3eb9b230267bc
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-03-09 11:29:28 +0530

    JS ADDed

---
+++ JS/mobile.js
@@ -0,0 +1,6 @@
+const mac_in_mobile = document.getElementById("macOS");
+const alert_container = document.getElementById("mobile_alert");
+function showMac() {
+    alert_container.style.display = "none";
+    mac_in_mobile.style.display = "block";
+}
